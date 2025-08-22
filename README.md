22ca781e45457b9779cbe456d579a19b178f1dc4
Amey Kulkarni <ameykulkarni@live.in>
2025-08-22 22:31:24 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Chess Next Move Checker
+
+Given a chess piece and its position on board, this program gives list of possible moves
+
+Usage:
+```python
+python chess_checker.py king a1
+```
---
+++ test_chess_checker.py
@@ -0,0 +1,11 @@
+# pawn in corner
+# pawn at the edge
+# pawn at the center
+# king in the corner
+# king on the edge
+# king at the center
+# queen in the corner
+# queen at the edge
+# queen at the center
+# piece in invalid position
+# invalid piece
