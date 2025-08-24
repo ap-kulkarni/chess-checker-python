29c320f9e252627b2c56c9de95e8147c9a75c418
Amey Kulkarni <ameykulkarni@live.in>
2025-08-24 16:12:14 +0530

    refactoring

--- chess_checker.py
+++ chess_checker.py
@@ -1,14 +1,16 @@
 import sys
 
+MAX_ROW = "8"
 
-def _get_row_and_column_from_position(position: str) -> tuple[str, str]:
+
+def _get_column_and_row_from_position(position: str) -> tuple[str, str]:
     return position[0], position[1]
 
 
 def get_next_moves(piece: str, current_position: str) -> list[str]:
-    column, row = _get_row_and_column_from_position(current_position)
+    column, row = _get_column_and_row_from_position(current_position)
     next_moves: list[str] = []
-    next_row = chr(min(ord(row) + 1, ord("8")))
+    next_row = chr(min(ord(row) + 1, ord(MAX_ROW)))
     next_move = f"{column}{next_row}"
     if next_move != current_position:
         next_moves.append(next_move)
