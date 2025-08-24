b9d11c5a7d3af929ac3f94c77a79028179511df5
Amey Kulkarni <ameykulkarni@live.in>
2025-08-24 19:19:44 +0530

    added code generate next moves for king

--- chess_checker.py
+++ chess_checker.py
@@ -1,20 +1,71 @@
 import sys
 
 MAX_ROW = "8"
+MIN_ROW = "1"
+MIN_COLUMN = "a"
+MAX_COLUMN = "h"
 
 
 def _get_column_and_row_from_position(position: str) -> tuple[str, str]:
     return position[0], position[1]
 
 
-def get_next_moves(piece: str, current_position: str) -> list[str]:
+def _get_row_with_offset(row: str, offset: int) -> str:
+    offset_row = chr(ord(row) + offset)
+    offset_row = MIN_ROW if offset_row < MIN_ROW else offset_row
+    offset_row = MAX_ROW if offset_row > MAX_ROW else offset_row
+    return offset_row
+
+
+def _get_column_with_offset(row: str, offset: int) -> str:
+    offset_column = chr(ord(row) + offset)
+    offset_column = MIN_COLUMN if offset_column < MIN_COLUMN else offset_column
+    offset_column = MAX_COLUMN if offset_column > MAX_COLUMN else offset_column
+    return offset_column
+
+
+def _get_next_move_with_offsets(
+    current_position: str, row_offset: int, column_offset: int
+):
     column, row = _get_column_and_row_from_position(current_position)
-    next_moves: list[str] = []
-    next_row = chr(min(ord(row) + 1, ord(MAX_ROW)))
-    next_move = f"{column}{next_row}"
-    if next_move != current_position:
-        next_moves.append(next_move)
-    return next_moves
+    next_possible_row = _get_row_with_offset(row, row_offset)
+    next_possible_column = _get_column_with_offset(column, column_offset)
+    return f"{next_possible_column}{next_possible_row}"
+
+
+def _update_moves_list_with_next_move(
+    moves_list: list[str], next_move: str, current_position: str
+) -> None:
+    if next_move not in moves_list and next_move != current_position:
+        moves_list.append(next_move)
+
+
+def _get_next_moves_for_pawn(current_position: str) -> list[str]:
+    possible_moves: list[str] = []
+    next_move = _get_next_move_with_offsets(current_position, 1, 0)
+    _update_moves_list_with_next_move(possible_moves, next_move, current_position)
+    return possible_moves
+
+
+def _get_next_moves_for_king(current_position: str) -> list[str]:
+    possible_moves: list[str] = []
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            next_move = _get_next_move_with_offsets(current_position, i, j)
+            _update_moves_list_with_next_move(
+                possible_moves, next_move, current_position
+            )
+    return possible_moves
+
+
+def get_next_moves(piece: str, current_position: str) -> list[str]:
+    if piece == "pawn":
+        return _get_next_moves_for_pawn(current_position)
+    if piece == "king":
+        return _get_next_moves_for_king(current_position)
+    return []
 
 
 if __name__ == "__main__":
--- test_chess_checker.py
+++ test_chess_checker.py
@@ -1,33 +1,88 @@
+import pytest
+
 from chess_checker import get_next_moves
+
+
+def _list_equals(list1: list[str], list2: list[str]) -> bool:
+    return set(list1) == set(list2)
 
 
 def test_pawn_in_lower_corner_has_one_possible_moves() -> None:
     got = get_next_moves("pawn", "a1")
     want = ["a2"]
-    assert got == want
+    assert _list_equals(got, want)
 
 
 def test_pawn_in_upper_corner_has_no_moves() -> None:
     got = get_next_moves("pawn", "h8")
     want: list[str] = []
-    assert got == want
+    assert _list_equals(got, want)
 
 
 def test_pawn_in_center_has_one_possible_move() -> None:
     got = get_next_moves("pawn", "d4")
     want = ["d5"]
-    assert got == want
+    assert _list_equals(got, want)
 
 
 def test_pawn_at_the_edge_has_one_possible_move() -> None:
     got = get_next_moves("pawn", "h4")
     want = ["h5"]
-    assert got == want
+    assert _list_equals(got, want)
 
 
 # king in the corner
-# king on the edge
-# king at the center
+@pytest.mark.parametrize(
+    "piece, position, output",
+    [
+        ("king", "a1", ["a2", "b1", "b2"]),
+        ("king", "h1", ["g1", "g2", "h2"]),
+        ("king", "a8", ["a7", "b7", "b8"]),
+        ("king", "h8", ["g8", "g7", "h7"]),
+    ],
+)
+def test_king_in_the_corner_has_three_possible_moves(
+    piece: str, position: str, output: list[str]
+) -> None:
+    got = get_next_moves(piece, position)
+    want = output
+    assert _list_equals(got, want)
+
+
+@pytest.mark.parametrize(
+    "piece, position, output",
+    [
+        ("king", "c3", ["b2", "b3", "b4", "c4", "d4", "d3", "d2", "c2"]),
+        ("king", "e5", ["d4", "d5", "d6", "e6", "f6", "f5", "f4", "e4"]),
+        ("king", "d5", ["c4", "c5", "c6", "d4", "d6", "e4", "e5", "e6"]),
+        ("king", "g7", ["f8", "g8", "h8", "h7", "h6", "g6", "f6", "f7"]),
+    ],
+)
+def test_king_in_the_middle_has_eight_possible_moves(
+    piece: str, position: str, output: list[str]
+) -> None:
+    got = get_next_moves(piece, position)
+    want = output
+    assert _list_equals(got, want)
+
+
+@pytest.mark.parametrize(
+    "piece, position, output",
+    [
+        ("king", "a3", ["a2", "a4", "b4", "b3", "b2"]),
+        ("king", "d1", ["c1", "c2", "d2", "e2", "e1"]),
+        ("king", "h6", ["g7", "h7", "h5", "g5", "g6"]),
+        ("king", "c8", ["d8", "d7", "c7", "b7", "b8"]),
+    ],
+)
+def test_king_at_the_edge_has_five_possible_moves(
+    piece: str, position: str, output: list[str]
+) -> None:
+    got = get_next_moves(piece, position)
+    want = output
+    assert _list_equals(got, want)
+
+
 # queen in the corner
 # queen at the edge
 # queen at the center
