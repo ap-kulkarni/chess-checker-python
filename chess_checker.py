f59ec48cedf32c7c571540fa7fc3b89b7838f4c0
Amey Kulkarni <ameykulkarni@live.in>
2025-08-24 10:48:42 +0530

    code to generate pawn's next moves

---
+++ chess_checker.py
@@ -0,0 +1,19 @@
+def _get_row_and_column_from_position(position: str) -> tuple[str, str]:
+    return position[0], position[1]
+
+
+def get_next_moves(piece: str, current_position: str) -> list[str]:
+    column, row = _get_row_and_column_from_position(current_position)
+    next_moves: list[str] = []
+    next_row = chr(min(ord(row) + 1, ord("8")))
+    next_move = f"{column}{next_row}"
+    if next_move != current_position:
+        next_moves.append(next_move)
+    return next_moves
+
+
+if __name__ == "__main__":
+    import sys
+
+    moves = get_next_moves(*sys.argv[1:])
+    print(",".join(moves))
--- test_chess_checker.py
+++ test_chess_checker.py
@@ -1,4 +1,25 @@
+from chess_checker import get_next_moves
+
+
 # pawn in corner
+def test_pawn_in_lower_corner_has_one_possible_moves() -> None:
+    got = get_next_moves("pawn", "a1")
+    want = ["a2"]
+    assert got == want
+
+
+def test_pawn_in_upper_corner_has_no_moves() -> None:
+    got = get_next_moves("pawn", "h8")
+    want: list[str] = []
+    assert got == want
+
+
+def test_pawn_in_center_has_one_possible_move() -> None:
+    got = get_next_moves("pawn", "d4")
+    want = ["d5"]
+    assert got == want
+
+
 # pawn at the edge
 # pawn at the center
 # king in the corner
